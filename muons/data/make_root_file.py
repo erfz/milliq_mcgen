a5c63c358d505f612e5d0df097387442298f3bde
claudiocc1 <Claudio.Campagnari@cern.ch>
2023-03-29 20:06:08 -0700

    muons run 3

--- muons/data/make_root_file.py
+++ muons/data/make_root_file.py
@@ -1,17 +1,27 @@
+# Some weird stuff here that does not seem to work in python 3
+# Also added the plot making at the end.
 import sys
 import numpy as np
 import ROOT as r
+import os
+import matplotlib.pyplot as plt
+
 
 fin = sys.argv[1]
 
 data = []
-with open(fin) as fid:
-    for line in fid:
-        if line.strip().startswith("#"):
-            continue
-        data.append(map(float, line.strip().split()))
+pyVersion = sys.version_info[0]
 
-data = np.array(data)
+if pyVersion == 2:
+    with open(fin) as fid:
+        for line in fid:
+            if line.strip().startswith("#"):
+                continue
+            data.append(map(float, line.strip().split()))
+        data = np.array(data)
+else:
+    data = np.loadtxt(fin)
+
 
 pt = data[:,0]
 cn = data[:,1]
@@ -41,5 +51,34 @@ h_dn.Write()
 fout.Close()
 
 
+# As a bonus, plot it as well
+fig, ax = plt.subplots()
+ax.plot(new_pt, new_up, linestyle='dashed', color='black',  label='up')
+ax.plot(new_pt, new_cn, linestyle='solid',  color='black', label='central')
+ax.plot(new_pt, new_dn, linestyle='dashed',  color='black', label='down')
+ax.fill_between(new_pt, new_dn, new_up, color="lightblue")
 
+ax.set_xlim(0, 300)
+ax.tick_params("both", direction='in', length=10, right=True, top=True)
+ax.tick_params("both", direction='in', length=7, right=True, which='minor')
+ax.set_yscale('log')
+ax.set_xlabel('Pt (Gev)')
+ax.set_ylabel('dsigma/dpt (pb/GeV)')
 
+# File name with extension but without path
+f1 = os.path.basename(fin)
+# file name witout extension but with path
+f2, _ = os.path.splitext(fin)
+# file name without extension and without path
+f3, _ = os.path.splitext(f1)
+
+ax.set_title(f3)
+fig.show()
+
+figFile = f2 + ".pdf" 
+blah = input("Enter y if you want to save the plot in pdf to %s   " % figFile)
+if blah == 'y':
+    print("Saving plot in ", figFile)
+    fig.savefig(figFile)
+else:
+    print("Plot not saved as pdf file")
